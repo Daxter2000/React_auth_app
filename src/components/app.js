ec520886c2a736808911e2693338befadca8c008
Alfredo <alfredo.hdez@hotmail.com>
2022-10-30 18:18:05 -0700

    Creating react app

---
+++ src/components/app.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+export default class App extends Component {
+  render() {
+    return (
+      <div className='app'>
+        <h1>DevCamp React Starter</h1>
+        <h2>React Skeleton</h2>
+        <h3>Hola como estan?</h3>
+      </div>
+    );
+  }
+}
